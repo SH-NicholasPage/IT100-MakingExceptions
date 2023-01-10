6d2afd69513901471ed1d13bbb0c8cda9f551864
Nicholas Page <npage@southhills.edu>
2023-01-09 20:37:51 -0500

    Initial commit

---
+++ IT100-ExceptionHomework/IT100-ExceptionHomework/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+* Name: [YOUR NAME HERE]
+* South Hills Username: [YOUR SOUTH HILLS USERNAME HERE]
+*/
+
+namespace IT100_ExceptionHomework
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            
+        }
+    }
+}
